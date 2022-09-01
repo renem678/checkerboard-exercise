1a4f27bc7d790f49ab33382e44aeac86826b8a2a
Rene Marquez <r.marquez678@gmail.com>
2022-08-31 19:19:51 -0500

    completed randomColor

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Your JS goes here
+let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+function createAndColor(color){
+let body = document.getElementsByTagName('body');
+let newSquare = document.createElement('div');
+newSquare.style.width= '11.1%'
+newSquare.style.paddingBottom = '11.1%'
+newSquare.style.cssFloat = 'left'
+newSquare.style.backgroundColor = color;
+document.body.appendChild(newSquare)
+}
+for (let i = 0; i < 81; i++){
+    if(i % 2 !== 0){
+        createAndColor('green');
+    } else if (i % 2 === 0){
+        createAndColor(randomColor);
+    }
+    
+    
+
+}
