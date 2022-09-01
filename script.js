013b335bc7ef74638d672eb941efb49848c1e562
Rene Marquez <r.marquez678@gmail.com>
2022-09-01 09:22:13 -0500

    finished Gradient

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 ;
-
+function addGradient(){
+    let gradient = document.createElement('div');
+    gradient.style.width = '100%';
+    gradient.style.height = '100%';
+    gradient.style.background = "linear-gradient(to right, rgba(67, 130, 53, 0.67), rgba(4, 198, 198, 0.62) 100%)";
+    gradient.style.zIndex = "2";
+    gradient.style.position = "fixed";
+    document.body.appendChild(gradient);
+}
 function createAndColor(color){
 let body = document.getElementsByTagName('body');
 let newSquare = document.createElement('div');
@@ -10,13 +18,15 @@ newSquare.style.cssFloat = 'left'
 newSquare.style.backgroundColor = color;
 document.body.appendChild(newSquare)
 }
+addGradient();
 for (let i = 0; i < 81; i++){
     if(i % 2 !== 0){
         createAndColor(`#${Math.floor(Math.random()*16777215).toString(16)}`);
     } else if (i % 2 === 0){
-        createAndColor((`#${Math.floor(Math.random()*16777215).toString(16)}`));
+        createAndColor(`#${Math.floor(Math.random()*16777215).toString(16)}`);
     }
     
     
 
 }
+
